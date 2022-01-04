e1977e466d8a5a9f025d06df82aa619d3ef52ebf
gladys5 <yumanji@live.com>
2022-01-04 11:37:15 -0700

    commitdos

--- src/App.js
+++ src/App.js
@@ -45,16 +45,16 @@ function App() {
         
            
             {weatherdata !==null ? (
-          <div className='main-container'>
+          <div className="main-container">
           <h4>Condiciones Climaticas</h4>
-          <div className='weather-icon'>
+          <div className="weather-icon">
            <img src={`http://openweathermap.org/img/w/${weatherdata.weather[0].icon}.png`} alt="imgicon"/>
           </div>
           <h3> {weatherdata.weather[0].main}</h3>
           <div className="temprature">
           <h1>{parseFloat(weatherdata.main.temp - 273.15).toFixed(1)}&deg;C</h1>
         </div>
-        <div className='location'>
+        <div className="location">
           <h3> <i className="fa fa-street-view"></i> {weatherdata.name}| {weatherdata.sys.country} </h3>
         </div>
         <div className="temprature-range">
